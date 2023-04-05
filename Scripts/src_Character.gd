8890aecd216f215d4dfdd909ab788667d93e1fd1
Stephen <ghub@stephenjackiw.33mail.com>
2023-04-05 14:18:10 -0400

    Character position can be set based on GridMap cells values

--- Scripts/src_Character.gd
+++ Scripts/src_Character.gd
@@ -6,16 +6,23 @@ const pfSPEED:int = 25;
 #Variables for functionality
 var canMove:bool = true;
 var currentGridLocation:Vector3i;
+@onready var gridMap = $"../../GridMap";
 
-func _setNextGridLocation(newGridLocation:Vector3i):
+func _setNextGridLocation(newGridLocation:Vector3i) -> void:
 	currentGridLocation = newGridLocation;
 #	print("Character - 01: Location updated.  The character is now at ", currentGridLocation, " on the GridMap."); #DEBUGGING
 	return;
 	
-func _updatePlayerLocation(targetPlayerLocation:Vector3):
+func _updatePlayerLocation(targetPlayerLocation:Vector3) -> void:
 	self.global_position = targetPlayerLocation;
-	print("Character - 01: Position updated.  The character is now at ", self.global_position, " in world space."); #DEBUGGING
+#	print("Character - 01: Position updated.  The character is now at ", self.global_position, " in world space."); #DEBUGGING
+	return;
+
+func _findPlayerPositionAt(gridCellLocation:Vector3i) -> Vector3:
+	var localLocationAtGridMapCell = gridMap.map_to_local(gridCellLocation);
+	var globalLocationAtGridMapCell = gridMap.to_global(localLocationAtGridMapCell);
+	return globalLocationAtGridMapCell;
 	
 func _process(delta):
-	_setNextGridLocation(Vector3i(0,0,0));
-	_updatePlayerLocation(Vector3(0,0,0));
+	_setNextGridLocation(Vector3i(0,1,2));
+	_updatePlayerLocation(_findPlayerPositionAt(currentGridLocation));
