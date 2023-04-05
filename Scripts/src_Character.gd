96a5e25d922df167cb23668899c881b3a76dc208
Stephen <ghub@stephenjackiw.33mail.com>
2023-04-05 19:07:00 -0400

    removed dead Character code

--- Scripts/src_Character.gd
+++ Scripts/src_Character.gd
@@ -1,24 +1,2 @@
 extends CharacterBody3D
 class_name playerCharacter;
-
-#var currentGridLocation:Vector3i;
-#@onready var gridMap = $"../../GridMap";
-#
-#func _setNextGridLocation(newGridLocation:Vector3i) -> void:
-#	currentGridLocation = newGridLocation;
-##	print("Character - 01: Location updated.  The character is now at ", currentGridLocation, " on the GridMap."); #DEBUGGING
-#	return;
-#
-#func _updatePlayerLocation(targetPlayerLocation:Vector3) -> void:
-#	self.global_position = targetPlayerLocation;
-##	print("Character - 01: Position updated.  The character is now at ", self.global_position, " in world space."); #DEBUGGING
-#	return;
-#
-#func _findPlayerPositionAt(gridCellLocation:Vector3i) -> Vector3:
-#	var localLocationAtGridMapCell = gridMap.map_to_local(gridCellLocation);
-#	var globalLocationAtGridMapCell = gridMap.to_global(localLocationAtGridMapCell);
-#	return globalLocationAtGridMapCell;
-#
-#func _process(delta):
-#	_setNextGridLocation(Vector3i(0,1,2));
-#	_updatePlayerLocation(_findPlayerPositionAt(currentGridLocation));
