4d7d127d47599fa40dc868159ca6e10f44428a94
Stephen <ghub@stephenjackiw.33mail.com>
2023-04-05 13:51:31 -0400

    Location setter method for player class is functional

--- Scripts/src_Character.gd
+++ Scripts/src_Character.gd
@@ -7,11 +7,15 @@ const pfSPEED:int = 25;
 var canMove:bool = true;
 var currentGridLocation:Vector3i;
 
-func _updateGridLocation(newGridLocation:Vector3i):
+func _setNextGridLocation(newGridLocation:Vector3i):
 	currentGridLocation = newGridLocation;
-#	self.global_position = currentGridLocation.
-	print("Character - 01: Location updated.  The character is now at ", currentGridLocation, " on the GridMap."); #DEBUGGING
+#	print("Character - 01: Location updated.  The character is now at ", currentGridLocation, " on the GridMap."); #DEBUGGING
 	return;
 	
+func _updatePlayerLocation(targetPlayerLocation:Vector3):
+	self.global_position = targetPlayerLocation;
+	print("Character - 01: Position updated.  The character is now at ", self.global_position, " in world space."); #DEBUGGING
+	
 func _process(delta):
-	_updateGridLocation(Vector3i(0,0,0));
+	_setNextGridLocation(Vector3i(0,0,0));
+	_updatePlayerLocation(Vector3(0,0,0));
